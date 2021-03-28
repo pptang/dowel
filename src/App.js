d1334e6c22992820d30b38b1e71c136f09836ef2
pptang <pptang@mercari.com>
2021-03-28 17:18:49 +0900

    feat: add privacy policy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Intro from './components/Intro/Intro';
 import Top from './components/Top/Top';
 import Problem from './components/Problem/Problem';
+import Privacy from './components/Privacy/Privacy';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Top />
-      <Problem />
-      <Intro />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            <Header />
+            <Top />
+            <Problem />
+            <Intro />
+          </div>
+        </Route>
+        <Route path="/privacy">
+          <Privacy />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
